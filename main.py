d6c8b056ca15fc192571dafb5cb581598411518d
RyosukeTakahashi <ramuniku@gmail.com>
2018-05-28 17:46:43 +0900

    port change

--- main.py
+++ main.py
@@ -1058,4 +1058,4 @@ add_group_event_handler()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=port, host='0.0.0.0')
